a9c7ed1dd2da36fc3d9875909a35d80dd9166b18
Robert Mînea <r.minea@student.tudelft.nl>
2019-02-27 16:25:27 +0200

    Add Calculator's first version

--- modules/Server/src/main/java/server/CarbonCalculator.java
+++ modules/Server/src/main/java/server/CarbonCalculator.java
@@ -2,54 +2,30 @@ package server;
 
 public class CarbonCalculator {
 
-    private double electricityPrice = 0.2;
-    private double naturalGasFactor = 120.61;
-    private double naturalGasPrice = 14.14;
-    private double oilFuelFactor = 22.37;
-    private double oilFuelPrice = 2.88;
-    private double propaneFactor = 12.17;
-    private double propanePrice = 2.88;
-    private double vehiclePerGallon = 19.4;
-    private double vehicleGreenhouse = 1.0526315;
-    private double publicPerGallon = 19.4;
+
     private double gramToPound = 0.0022;
-    private double airPerMile = 223;
-    private double wellToPumpFactor = 1.2;
-    private double radiativeForcing = 1.9;
-    private double meatFactor = 1452;
-    private double cerealsFactor = 741;
-    private double dairyFactor = 1911;
-    private double fruitFactor = 368;
-    private double otherFoodFactor = 467;
-    private double clothingFactor = 436;
-    private double furnishHouseholdFactor = 459;
-    private double otherGoodsFactor = 338;
-    private double serviceFactor = 178;
 
 
-    // 1 for euro, 2 for dollar
-    private int currency;
+
+
     // 1 for pounds, 2 for grams
     private int metrics;
 
-    public CarbonCalculator(int currency, int metrics) {
-        this.currency = currency;
+    public CarbonCalculator(int metrics) {
         this.metrics = metrics;
     }
 
     public double electricity(double amount, double electricityPrice) {
         double electricityFactor = 1.37;
+        double averageElectricityPrice = 0.2;
 
-        if (currency == 1) {
-            amount = convertToDollar(amount);
-        }
         if (electricityPrice == 0) {
-            electricityPrice = this.electricityPrice;
+            electricityPrice = averageElectricityPrice;
         }
         double toReturn = (amount / electricityPrice) * electricityFactor * 12;
 
         if (metrics == 2) {
-            return poundsToGrams(toReturn);
+            return poundsToKilograms(toReturn);
         }
 
         return toReturn;
@@ -57,94 +33,180 @@ public class CarbonCalculator {
     }
 
     public double naturalGas(double amount, double naturalGasPrice) {
-        if (currency == 1) {
-            amount = convertToDollar(amount);
-        }
+        double naturalGasFactor = 120.61;
+        double averageNaturalGasPrice = 14.14;
 
         if (naturalGasPrice == 0) {
-            naturalGasPrice = this.naturalGasPrice;
+            naturalGasPrice = averageNaturalGasPrice;
         }
 
         double toReturn = (amount / naturalGasPrice) * naturalGasFactor * 12;
 
         if (metrics == 2) {
-            return poundsToGrams(toReturn);
+            return poundsToKilograms(toReturn);
         }
 
         return toReturn;
     }
 
     public double fuelOil(double amount, double oilFuelPrice) {
-        if (currency == 1) {
-            amount = convertToDollar(amount);
-        }
+        double oilFuelFactor = 22.37;
+        double averageOilFuelPrice = 2.88;
 
         if (oilFuelPrice == 0) {
-            oilFuelPrice = this.oilFuelPrice;
+            oilFuelPrice = averageOilFuelPrice;
         }
 
         double toReturn = (amount / oilFuelPrice) * oilFuelFactor * 12;
 
         if (metrics == 2) {
-            return poundsToGrams(toReturn);
+            return poundsToKilograms(toReturn);
         }
 
         return toReturn;
     }
 
     public double propane(double amount, double propanePrice) {
-        if (currency == 1) {
-            amount = convertToDollar(amount);
-        }
+        double propaneFactor = 12.17;
+        double averagePropanePrice = 2.88;
+
         if (propanePrice == 0) {
-            propanePrice = this.propanePrice;
+            propanePrice = averagePropanePrice;
         }
 
         double toReturn = (amount / propanePrice) * propaneFactor * 12;
 
         if (metrics == 2) {
-            return poundsToGrams(toReturn);
+            return poundsToKilograms(toReturn);
         }
 
         return toReturn;
     }
 
     public double vehicle(double amount, double milesPerGallon) {
-        if (currency == 1) {
-            amount = convertToDollar(amount);
-        }
+        double vehiclePerGallon = 19.4;
+        double vehicleGreenhouse = 1.0526315;
 
-        double toReturn = ((amount * 52.117) / milesPerGallon) * 19.4 * vehicleGreenhouse;
+
+        double toReturn = ((amount * 52.117) / milesPerGallon)
+                            * vehiclePerGallon * vehicleGreenhouse;
 
         if (metrics == 2) {
-            return poundsToGrams(toReturn);
+            return poundsToKilograms(toReturn);
         }
 
         return toReturn;
     }
 
-    //TO DO LATER
-    //public double publicTransport(double amount)
+
+    public double publicTransport(double amount) {
+        double publicPerGallon = 19.4;
+        double averageMilesPerGallon = 38.3;
+
+        double toReturn = (amount / averageMilesPerGallon) * publicPerGallon;
+
+        if (metrics == 2) {
+            return poundsToKilograms(toReturn);
+        }
+
+        return toReturn;
+    }
+
 
     public double airTravel(double milesPerYear) {
+        double airPerMile = 223;
+        double wellToPumpFactor = 1.2;
+        double radiativeForcing = 1.9;
 
 
-        double toReturn = milesPerYear * (airPerMile * wellToPumpFactor * radiativeForcing) * gramToPound;
+        double toReturn = milesPerYear * (airPerMile * wellToPumpFactor * radiativeForcing)
+                            * gramToPound;
 
         if (metrics == 2) {
-            return poundsToGrams(toReturn);
+            return poundsToKilograms(toReturn);
         }
 
         return toReturn;
     }
 
-    public double poundsToGrams(double pounds) {
-        return pounds * 2;
+    public double food(double amount, int type) {
+        if (type > 6 || type < 1) {
+            throw new IllegalArgumentException("Please insert a valid type of Food!");
+        }
+
+        double specificFactor = 0;
+
+        switch (type) {
+            // meatFactor = 1452
+            case 1: specificFactor = 1452;
+            break;
+            // cerealsFactor = 741
+            case 2: specificFactor = 741;
+            break;
+            // dairyFactor = 1911
+            case 3: specificFactor = 1911;
+            break;
+            // fruitFactor = 1176
+            case 4: specificFactor = 368;
+            break;
+            // eatingOutFactor = 368
+            case 5: specificFactor = 368;
+            break;
+            // otherFoodFactor = 467
+            case 6: specificFactor = 467;
+            break;
+            default: break;
+        }
+
+        double toReturn = (amount * specificFactor * 12) * gramToPound;
+
+        if (metrics == 2) {
+            return poundsToKilograms(toReturn);
+        }
+
+        return toReturn;
+    }
+
+    public double servicesAndGoods(double amount, int type) {
+        if (type > 4 || type < 1) {
+            throw new IllegalArgumentException("Please insert a valid type of Service/Goods!");
+        }
+
+        double specificFactor = 0;
+
+        switch (type) {
+            // clothingFactor = 436
+            case 1: specificFactor = 436;
+            break;
+            // furnishHouseholdFactor = 459
+            case 2: specificFactor = 459;
+            break;
+            // otherGoodsFactor = 338
+            case 3: specificFactor = 338;
+            break;
+            // serviceFactor = 178
+            case 4: specificFactor = 178;
+            break;
+            default: break;
+        }
+
+        double toReturn = (amount * specificFactor * 12) * gramToPound;
+
+        if (metrics == 2) {
+            return poundsToKilograms(toReturn);
+        }
+
+        return toReturn;
+    }
+
+
+    public double poundsToKilograms(double pounds) {
+        return pounds * 0.45359237;
     }
 
 
 
-    public double convertToDollar(double amount) {
-        return amount*1.137436;
+    public static void main(String[] args){
+
     }
 }
