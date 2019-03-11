0e7da8aeeb66e0e69fb1e7ff9d7dcab3e0af7948
Irem Ugurlu <Ugurlu@student.tudelft.nl>
2019-03-11 17:08:43 +0100

    Add resources and controllers for javafx

---
+++ modules/Client/src/main/java/GUI/LoginPageController.java
@@ -0,0 +1,80 @@
+package GUI;
+
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Window;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
+public class LoginPageController {
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+    @FXML
+    private GridPane anchorRoot;
+    @FXML
+    private GridPane parentContainer;
+
+
+    @FXML
+    private void loadSecond(ActionEvent event) throws IOException {
+        Window owner = loginButton.getScene().getWindow();
+        if(nameField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Log-in Error!",
+                    "Please enter your username");
+            return;
+        }
+        if(passwordField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Log-in Error!",
+                    "Please enter a password");
+            return;
+        }
+
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("dashboard.fxml"));
+        Scene scene = loginButton.getScene();
+        root.translateYProperty().set(scene.getHeight());
+
+        parentContainer.getChildren().add(root);
+
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(1), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            parentContainer.getChildren().remove(anchorRoot);
+        });
+        timeline.play();
+    }
+
+    public static class AlertHelper {
+        public static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+            Alert alert = new Alert(alertType);
+            alert.setTitle(title);
+            alert.setHeaderText(null);
+            alert.setContentText(message);
+            alert.initOwner(owner);
+            alert.show();
+        }
+    }
+
+}
+
