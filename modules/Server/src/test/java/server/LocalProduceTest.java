d624af8d0dbad0bdefbf6ea973ab1193ca31f761
jaron rosenberg <jrosenberg@student.tudelft.nl>
2019-03-15 12:42:58 +0000

    reformat and update the test for localproduce

--- modules/Server/src/test/java/server/LocalProduceTest.java
+++ modules/Server/src/test/java/server/LocalProduceTest.java
@@ -1,8 +1,5 @@
 package server;
 
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-
-import cn.hutool.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,15 +11,10 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.sql.*;
 
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 
 @RunWith(PowerMockRunner.class)
@@ -71,24 +63,25 @@ public class LocalProduceTest {
 
     /**
      * Test for the /localproduce/totalVegan endpoint.
-     * @throws SQLException SQL error
+     *
+     * @throws SQLException           SQL error
      * @throws ClassNotFoundException Class not found error
      */
     @Test
-    public void getData() throws SQLException, ClassNotFoundException {
+    public void getAll() throws SQLException, ClassNotFoundException {
         localProduce = new LocalProduce();
         mockStatic(DriverManager.class);
         Mockito.when(DriverManager
                 .getConnection(
                         "jdbc:mysql://localhost:3306/greener?autoReconnect=true&useSSL=false",
-                        "sammy","temporary")).thenReturn(mockConnection);
+                        "sammy", "temporary")).thenReturn(mockConnection);
 
         Mockito.when(mockConnection.createStatement()).thenReturn(mockStatement);
         Mockito.when(mockStatement
                 .executeQuery("SELECT Local_produce FROM person WHERE Name = 'Robert'"))
                 .thenReturn(rs);
 
-        Mockito.when(rs.getInt("Local_produce")).thenReturn(1);
+        Mockito.when(rs.getInt("Vegan_meal")).thenReturn(1);
         Mockito.when(rs.next()).thenReturn(true);
         Response value = localProduce.getData();
 
@@ -97,121 +90,4 @@ public class LocalProduceTest {
         Assert.assertEquals(value.getEntity().toString(), "{\"total\":1}");
         //Mockito.verify(mockConnection.createStatement(), Mockito.times(1));
     }
-
 }
-
-
-//    private LocalProduce lp;
-//    private JSONObject joCor; //correct JSON object for response testing
-//    private JSONObject joIncor; //incorrect JSON object for response testing
-//
-//
-//    /**
-//     * Sets up all the variables
-//     * needed for testing before
-//     * the tests are executed.
-//     */
-//    @Before
-//    public void setup() {
-//        lp = new LocalProduce();
-//
-//        joCor = new JSONObject();
-//        joCor.append("Weight", "100");
-//        joIncor = new JSONObject();
-//        joIncor.append("Weight", "200");
-//    }
-//
-//    /**
-//     * Tests the equality between an arbitrary
-//     * JSON value equal to the pre-set
-//     * value in GET response body.
-//     * Expects true.
-//     */
-//    @Test
-//    public void getDataCorrectEntity() {
-//        Assert.assertEquals(lp.getData().getEntity(),
-//                joCor);
-//    }
-//
-//    /**
-//     * Tests the equality between an arbitrary
-//     * JSON value equal to the pre-set
-//     * value in GET response body.
-//     * Expects false.
-//     */
-//    @Test
-//    public void getDataIncorrectEntity() {
-//        Assert.assertNotEquals(lp.getData().getEntity(),
-//                joIncor);
-//    }
-//
-//    /**
-//     * Tests whether the GET method
-//     * returns a response with an
-//     * OK Status (200).
-//     * Expects true.
-//     */
-//    @Test
-//    public void getDataCorrectStatus() {
-//        Assert.assertEquals(lp.getData().getStatus(), 200);
-//    }
-//
-//    /**
-//     * Tests whether the GET method
-//     * returns a response with an
-//     * OK Status (200).
-//     * Expects false.
-//     */
-//    @Test
-//    public void getDataIncorrectStatus() {
-//        Assert.assertNotEquals(lp.getData().getStatus(), 404);
-//    }
-//
-//    /**
-//     * Tests the equality between an arbitrary
-//     * JSON value equal to the pre-set
-//     * value in POST response body.
-//     * Expects true.
-//     */
-//    @Test
-//    public void postDataCorrectEntity() {
-//        Assert.assertEquals(lp.postData(joCor).getEntity(),
-//                joCor);
-//    }
-//
-//    /**
-//     * Tests the equality between an arbitrary
-//     * JSON value equal to the pre-set
-//     * value in POST response body.
-//     * Expects false.
-//     */
-//    @Test
-//    public void postDataIncorrectEntity() {
-//        Assert.assertNotEquals(lp.postData(joCor).getEntity(),
-//                joIncor);
-//    }
-//
-//    /**
-//     * Tests whether the POST method
-//     * returns a response with an
-//     * OK Status (200).
-//     * Expects true.
-//     */
-//    @Test
-//    public void postDataCorrectStatus() {
-//        Assert.assertEquals(lp.postData(joCor).getStatus(),
-//                200);
-//    }
-//
-//    /**
-//     * Tests whether the POST method
-//     * returns a response with an
-//     * OK Status (200).
-//     * Expects false.
-//     */
-//    @Test
-//    public void postDataIncorrectStatus() {
-//        Assert.assertNotEquals(lp.postData(joCor).getStatus(),
-//                404);
-//    }
-//}
