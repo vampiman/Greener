031963bbfd1d3eb4d27addbb5e20cc6c96eb8432
Lee Chen <l.s.chen@student.tudelft.nl>
2019-03-15 15:21:09 +0000

    Extend Bike.java

--- modules/Server/src/main/java/server/Bike.java
+++ modules/Server/src/main/java/server/Bike.java
@@ -11,6 +11,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.*;
 
 /**
  * Root resource (exposed at "bike" path).
@@ -19,6 +20,19 @@ import javax.ws.rs.core.Response;
 @Singleton
 @Resource
 public class Bike {
+
+    private Connection dbConnection;
+
+    /**
+     * This method initializes the connection with the database server through jdbc.
+     */
+    public void getDbConneciton() throws ClassNotFoundException, SQLException{
+        String url = "jdbc:mysql://localhost:3306/greener?autoReconnect=true&useSSL=false";
+        String user = "sammy";
+        String pass = "temporary";
+
+        dbConnection = DriverManager.getConnection(url, user, pass);
+    }
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
@@ -29,11 +43,27 @@ public class Bike {
     @GET
     @Path("get")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getData() {
+    public Response getData() throws ClassNotFoundException, SQLException{
+
+        getDbConneciton();
+
+        Statement st = dbConnection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT  Bike FROM person WHERE Name = 'Robert'");
+        rs.next();
+        int total = rs.getInt("Bike");
+        st.close();
+        dbConnection.close();
         JSONObject jo = new JSONObject();
-        jo.append("Distance", "10");
+        jo.put("total", total);
+        dbConnection.close();
+        st.close();
         return Response.status(Response.Status.OK).entity(jo).build();
     }
+    //    public Response getData() {
+//        JSONObject jo = new JSONObject();
+//        jo.append("Weight", "100");
+//        return Response.status(Response.Status.OK).entity(jo).build();
+//    }
 
     /**
      * Method handling HTTP POST requests. It accepts the JSON
@@ -44,7 +74,17 @@ public class Bike {
     @POST
     @Path("post")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response postData(JSONObject jo) {
-        return Response.status(200).entity(jo).build();
+
+    public void postData() throws ClassNotFoundException, SQLException{
+        getDbConneciton();
+        Statement st = dbConnection.createStatement();
+        st.executeUpdate("UPDATE person SET Bike = Bike + 1 WHERE Name = 'Robert'");
+
+        st.close();
+        dbConnection.close();
     }
+
+    //public Response postData(JSONObject jo) {
+        //return Response.status(200).entity(jo).build();
+    //}
 }
