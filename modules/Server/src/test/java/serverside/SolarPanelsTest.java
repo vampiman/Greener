e0ff7f89320f936ec86bb8f47cd77b74274c4885
jaron rosenberg <jrosenberg@student.tudelft.nl>
2019-03-16 14:57:20 +0000

    Upload correct solar panels test with correct encoding

---
+++ modules/Server/src/test/java/serverside/SolarPanelsTest.java
@@ -0,0 +1,98 @@
+package serverside;
+
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.ws.rs.core.Response;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(SolarPanels.class)
+public class SolarPanelsTest {
+
+
+    @Mock
+    private Connection mockConnection;
+    @Mock
+    private Statement mockStatement;
+    @Mock
+    private ResultSet rs;
+
+    @InjectMocks
+    private SolarPanels solarPanels;
+
+    /**
+     * Setup method for the test methods that depend on mocks.
+     */
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockConnection = Mockito.mock(Connection.class);
+        mockStatement = Mockito.mock(Statement.class);
+        rs = Mockito.mock(ResultSet.class);
+
+    }
+
+    /**
+     * Test for the /solarpanels/post endpoint.
+     *
+     * @throws SQLException           SQL error
+     * @throws ClassNotFoundException Class not found error
+     */
+    @Test
+    public void postAmount() throws SQLException, ClassNotFoundException {
+        solarPanels = new SolarPanels();
+        mockStatic(DriverManager.class);
+        Mockito.when(DriverManager
+                .getConnection(
+                        "jdbc:mysql://localhost:3306/greener?autoReconnect=true&useSSL=false",
+                        "sammy", "temporary")).thenReturn(mockConnection);
+        Mockito.when(mockConnection.createStatement()).thenReturn(mockStatement);
+        solarPanels.postAmount();
+
+    }
+
+    /**
+     * Test for the /localproduce/totalproduce endpoint.
+     *
+     * @throws SQLException           SQL error
+     * @throws ClassNotFoundException Class not found error
+     */
+    @Test
+    public void getAmount() throws SQLException, ClassNotFoundException {
+        solarPanels = new SolarPanels();
+        mockStatic(DriverManager.class);
+        Mockito.when(DriverManager
+                .getConnection(
+                        "jdbc:mysql://localhost:3306/greener?autoReconnect=true&useSSL=false",
+                        "sammy", "temporary")).thenReturn(mockConnection);
+
+        Mockito.when(mockConnection.createStatement()).thenReturn(mockStatement);
+        Mockito.when(mockStatement
+                .executeQuery("SELECT Solar_panels FROM person WHERE Name = 'Robert'"))
+                .thenReturn(rs);
+
+        Mockito.when(rs.getInt("Solar_panels")).thenReturn(1);
+        Mockito.when(rs.next()).thenReturn(true);
+        Response value = solarPanels.getAmount();
+
+        System.out.println(value.getEntity());
+        Assert.assertEquals(value.getEntity().toString(), "{\"total\":1}");
+    }
+}
