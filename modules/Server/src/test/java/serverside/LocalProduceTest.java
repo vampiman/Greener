da45c7691551d3dc30dbde39ffed27296aed036f
jaron rosenberg <jrosenberg@student.tudelft.nl>
2019-03-18 09:56:46 +0000

    Update LocalProduceTest.java to get the right tests up

--- modules/Server/src/test/java/serverside/LocalProduceTest.java
+++ modules/Server/src/test/java/serverside/LocalProduceTest.java
@@ -1,123 +1,92 @@
 package serverside;
 
-import cn.hutool.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import javax.ws.rs.core.Response;
+import java.sql.*;
+
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(LocalProduce.class)
 public class LocalProduceTest {
 
-    private LocalProduce lp;
-    private JSONObject joCor; //correct JSON object for response testing
-    private JSONObject joIncor; //incorrect JSON object for response testing
+    @Mock
+    private Connection mockConnection;
+    @Mock
+    private Statement mockStatement;
+    @Mock
+    private ResultSet rs;
 
+    @InjectMocks
+    private LocalProduce localProduce;
 
     /**
-     * Sets up all the variables
-     * needed for testing before
-     * the tests are executed.
+     * Setup method for the test methods that depend on mocks.
      */
     @Before
-    public void setup() {
-        lp = new LocalProduce();
-
-        joCor = new JSONObject();
-        joCor.append("Weight", "100");
-        joIncor = new JSONObject();
-        joIncor.append("Weight", "200");
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockConnection = Mockito.mock(Connection.class);
+        mockStatement = Mockito.mock(Statement.class);
+        rs = Mockito.mock(ResultSet.class);
     }
 
     /**
-     * Tests the equality between an arbitrary
-     * JSON value equal to the pre-set
-     * value in GET response body.
-     * Expects true.
+     * Test for the /localproduce/post endpoint.
+     *
+     * @throws SQLException           SQL error
+     * @throws ClassNotFoundException Class not found error
      */
+
     @Test
-    public void getDataCorrectEntity() {
-        Assert.assertEquals(lp.getData().getEntity(),
-                joCor);
+    public void postData() throws SQLException, ClassNotFoundException {
+        localProduce = new LocalProduce();
+        mockStatic(DriverManager.class);
+        Mockito.when(DriverManager
+                .getConnection(
+                        "jdbc:mysql://localhost:3306/greener?autoReconnect=true&useSSL=false",
+                        "sammy", "temporary")).thenReturn(mockConnection);
+        Mockito.when(mockConnection.createStatement()).thenReturn(mockStatement);
+        localProduce.postData();
     }
 
     /**
-     * Tests the equality between an arbitrary
-     * JSON value equal to the pre-set
-     * value in GET response body.
-     * Expects false.
+     * Test for the /localproduce/totalProduce endpoint.
+     *
+     * @throws SQLException           SQL error
+     * @throws ClassNotFoundException Class not found error
      */
     @Test
-    public void getDataIncorrectEntity() {
-        Assert.assertNotEquals(lp.getData().getEntity(),
-                joIncor);
-    }
+    public void getAll() throws SQLException, ClassNotFoundException {
+        localProduce = new LocalProduce();
+        mockStatic(DriverManager.class);
+        Mockito.when(DriverManager
+                .getConnection(
+                        "jdbc:mysql://localhost:3306/greener?autoReconnect=true&useSSL=false",
+                        "sammy", "temporary")).thenReturn(mockConnection);
 
-    /**
-     * Tests whether the GET method
-     * returns a response with an
-     * OK Status (200).
-     * Expects true.
-     */
-    @Test
-    public void getDataCorrectStatus() {
-        Assert.assertEquals(lp.getData().getStatus(), 200);
-    }
+        Mockito.when(mockConnection.createStatement()).thenReturn(mockStatement);
+        Mockito.when(mockStatement
+                .executeQuery("SELECT Local_produce FROM person WHERE Name = 'Robert'"))
+                .thenReturn(rs);
 
-    /**
-     * Tests whether the GET method
-     * returns a response with an
-     * OK Status (200).
-     * Expects false.
-     */
-    @Test
-    public void getDataIncorrectStatus() {
-        Assert.assertNotEquals(lp.getData().getStatus(), 404);
-    }
+        Mockito.when(rs.getInt("Local_produce")).thenReturn(1);
+        Mockito.when(rs.next()).thenReturn(true);
+        Response value = localProduce.getData();
 
-    /**
-     * Tests the equality between an arbitrary
-     * JSON value equal to the pre-set
-     * value in POST response body.
-     * Expects true.
-     */
-    @Test
-    public void postDataCorrectEntity() {
-        Assert.assertEquals(lp.postData(joCor).getEntity(),
-                joCor);
-    }
 
-    /**
-     * Tests the equality between an arbitrary
-     * JSON value equal to the pre-set
-     * value in POST response body.
-     * Expects false.
-     */
-    @Test
-    public void postDataIncorrectEntity() {
-        Assert.assertNotEquals(lp.postData(joCor).getEntity(),
-                joIncor);
-    }
-
-    /**
-     * Tests whether the POST method
-     * returns a response with an
-     * OK Status (200).
-     * Expects true.
-     */
-    @Test
-    public void postDataCorrectStatus() {
-        Assert.assertEquals(lp.postData(joCor).getStatus(),
-                200);
-    }
-
-    /**
-     * Tests whether the POST method
-     * returns a response with an
-     * OK Status (200).
-     * Expects false.
-     */
-    @Test
-    public void postDataIncorrectStatus() {
-        Assert.assertNotEquals(lp.postData(joCor).getStatus(),
-                404);
-    }
+        System.out.println(value.getEntity());
+        Assert.assertEquals(value.getEntity().toString(), "{\"total\":1}");
+     }
 }
