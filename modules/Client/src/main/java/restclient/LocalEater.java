138cf8892fe70b4f28a4780c03b5eb21d5800f4a
jaron rosenberg <jrosenberg@student.tudelft.nl>
2019-03-20 17:22:18 +0000

    Update LocalEater.java to pass

--- modules/Client/src/main/java/restclient/LocalEater.java
+++ modules/Client/src/main/java/restclient/LocalEater.java
@@ -1,204 +1,97 @@
 package restclient;
 
-import static org.mockito.Mockito.when;
-
 import cn.hutool.json.JSONObject;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+
+//import java.util.Scanner;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+public class LocalEater {
 
+    protected Client client;
 
-public class LocalEaterTest {
-
-    @Mock
-    Client client;
-
-    @Mock
-    Response resp;
-
-    @Mock
-    JSONObject jo;
-
-    @Mock
-    WebTarget webTarget;
-
-    @Mock
-    Invocation.Builder ib;
-
-    @InjectMocks
-    LocalEater localEater;
-
-    /**
-     * Setup for the following mock-dependant tests.
-     */
-    @Before
-    public void setup() {
-        client = Mockito.mock(Client.class);
-        resp = Mockito.mock(Response.class);
-        jo = Mockito.mock(JSONObject.class);
-        webTarget = Mockito.mock(WebTarget.class);
-        ib = Mockito.mock(Invocation.Builder.class);
-        localEater = new LocalEater(client);
-
-        when(client.target("http://localhost:8080/serverside/webapi/localproduce/totalProduce")).thenReturn(webTarget);
-        when(client.target("http://localhost:8080/serverside/webapi/localproduce/post")).thenReturn(webTarget);
-        when(webTarget.request(MediaType.APPLICATION_JSON)).thenReturn(ib);
-        when(ib.get(Response.class)).thenReturn(resp);
-
-        when(resp.readEntity(JSONObject.class)).thenReturn(jo);
-        when(jo.getInt("total")).thenReturn(1);
-
-    }
-    //NEED TO ADD SOME RETURN MESSAGE
-
-    /**
-     * Test for the sendVeganMeal method.
-     */
-    @Test
-    public void sendLocalEater() {
-        localEater.sendLocal(1);
+    public LocalEater(Client client) {
+        this.client = client;
     }
 
     /**
-     * Test for the getTotalVeganMeals method.
+     * The method creates a client and a GET request
+     * for retrieval of data on the serverside in a JSON file.
+     *
+     * @return JSON object contained in the serverside response.
      */
-    @Test
-    public void getTotalProduce() {
-        int result = localEater.getTotalProduce();
-        Assert.assertEquals(1, result);
+    public JSONObject getActivityInfo(String uri) {
+        WebTarget webTarget = this.client.target(uri);
+
+        Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+        Response response = invocationBuilder.get(Response.class);
+
+        JSONObject jo = response.readEntity(JSONObject.class);
+//        System.out.println(jo.toJSONString(10));
+        return jo;
+
     }
+
+    /**
+     * The method creates a client and a POST request
+     * for upload of the entered data to the serverside as JSON.
+     *
+     * @return the JSON object from serverside response (the one which was posted)
+     */
+
+    public void sendLocal(int total){
+        JSONObject jo = new JSONObject();
+        jo.put("total", total);
+
+        Resource vm = new Resource();
+        vm.setTotal_Produce(total);
+
+        this.client.target("http://localhost:8080/serverside/webapi/localproduce/post")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(vm));
+
+    }
+//    public JSONObject postActivityInfo(String uri) {
+//        JSONObject j1 = new JSONObject().append("Weight", "100");
+//        JSONObject j2 = client.target(uri)
+//                .request(MediaType.APPLICATION_JSON)
+//                .post(Entity.json(j1))
+//                .readEntity(JSONObject.class);
+////        System.out.println(j2.toJSONString(10));
+//        return j2;
+//    }
+    /**
+     * Method for sending a JSON-based request to the serverside in order to retrieve
+     * the total number of bought local products.
+     * @return Total number of local produce
+     */
+    public int getTotalProduce() {
+        //Client client =  ClientBuilder.newClient();
+        Response resp = this.client.target("http://localhost:8080/serverside/webapi/localproduce/totalProduce")
+                .request(MediaType.APPLICATION_JSON)
+                .get(Response.class);
+
+
+        JSONObject jo = resp.readEntity(JSONObject.class);
+
+        return jo.getInt("total");
+    }
+    //      Used for testing only
+    //    /**
+    //     * Main method that simulates the client.
+    //     *
+    //     * @param args Input for main
+    //     */
+    //    public static void main(String[] args) {
+    //        LocalEater le = new LocalEater(ClientBuilder.newClient());
+    //
+    //        le.getActivityInfo("http://localhost:8080/server/webapi/localproduce/get");
+    //        le.postActivityInfo("http://localhost:8080/server/webapi/localproduce/post");
+    //    }
+
 }
-
-
-//package restclient;
-//
-//import static org.mockito.ArgumentMatchers.anyString;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.when;
-//
-//import cn.hutool.json.JSONObject;
-//
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//
-//import javax.ws.rs.client.Client;
-//import javax.ws.rs.client.Entity;
-//import javax.ws.rs.client.Invocation;
-//import javax.ws.rs.client.WebTarget;
-//import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.core.Response;
-//
-//public class LocalEaterTest {
-//
-//
-//    @Mock
-//    WebTarget target;
-//
-//    @Mock
-//    Invocation.Builder builder;
-//
-//    @Mock
-//    Client client;
-//
-//    @Mock
-//    Response res;
-//
-//    @InjectMocks
-//    LocalEater leClient;
-//
-//
-//    /**
-//     * Method that initialises all objects needed for testing
-//     * and specifies mocks' behaviour.
-//     */
-//    @Before
-//    public void setup() {
-//        client = mock(Client.class);
-//        leClient = new LocalEater(client);
-//
-//        JSONObject jo = new JSONObject();
-//        jo.append("Weight", "100");
-//        res = mock(Response.class);
-//
-//        when(res.readEntity(JSONObject.class)).thenReturn(jo);
-//        when(res.getStatus()).thenReturn(200); //The OK status
-//
-//        builder = mock(Invocation.Builder.class);
-//        when(builder.get(Response.class)).thenReturn(res);
-//        when(builder.post(Entity.json(jo))).thenReturn(res);
-//
-//        target = mock(WebTarget.class);
-//        when(target.path(anyString())).thenReturn(target);
-//        when(target.request(MediaType.APPLICATION_JSON)).thenReturn(builder);
-//        when(leClient.client.target("testLocalEater")).thenReturn(target);
-//
-//    }
-//
-//    /**
-//     * Tests the equality between an arbitrary
-//     * JSON value equal to the pre-set
-//     * value in GET response body.
-//     * Expects true.
-//     */
-//    @Test
-//    public void getRequestCorrect() {
-//        JSONObject jo = new JSONObject();
-//        jo.append("Weight", "100");
-//        Assert.assertEquals(leClient.getActivityInfo("testLocalEater")
-//                .toJSONString(10), jo.toJSONString(10));
-//    }
-//
-//    /**
-//     * Tests the (in)equality between an arbitrary
-//     * JSON value not equal to the pre-set
-//     * value in GET response body.
-//     * Expects false.
-//     */
-//    @Test
-//    public void getRequestIncorrect() {
-//        JSONObject jo = new JSONObject();
-//        jo.append("W", "200g");
-//        Assert.assertNotEquals(leClient.getActivityInfo("testLocalEater")
-//                .toJSONString(10), jo.toJSONString(10));
-//    }
-//
-//    /**
-//     * Tests (in)equality between an arbitrary value of distance
-//     * equal to the pre-set value in the post method.
-//     * Expects true.
-//     */
-//    @Test
-//    public void postRequestCorrect() {
-//        JSONObject j1 = new JSONObject();
-//        j1.append("Weight", "100");
-//        JSONObject j2 = leClient.postActivityInfo("testLocalEater");
-//        Assert.assertEquals(j2.toJSONString(10), j1.toJSONString(10));
-//    }
-//
-//    /**
-//     * Tests (in)equality between an arbitrary value of distance
-//     * not equal to the pre-set value in the POST method.
-//     * Expects false.
-//     */
-//    @Test
-//    public void postRequestIncorrect() {
-//        JSONObject j1 = new JSONObject();
-//        j1.append("Wieght", "2050");
-//        JSONObject j2 = leClient.postActivityInfo("testLocalEater");
-//        Assert.assertNotEquals(j2.toJSONString(10), j1.toJSONString(10));
-//    }
-//
-//}
