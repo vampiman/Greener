396fe5716552d62969908b8ef363dd46fa785f73
jaron rosenberg <jrosenberg@student.tudelft.nl>
2019-03-21 11:16:13 +0000

    Add missing resource tests ResourceTest.java

--- modules/Client/src/test/java/restclient/ResourceTest.java
+++ modules/Client/src/test/java/restclient/ResourceTest.java
@@ -9,7 +9,7 @@ public class ResourceTest {
      * Test for the "total" field getter.
      */
     @Test
-    public void getTotal() {
+    public void getTotal_Maels() {
         Resource vm = new Resource();
         vm.setTotal_Meals(2);
         Assert.assertEquals(vm.getTotal_Meals(),2);
@@ -19,9 +19,40 @@ public class ResourceTest {
      * Test for the "total" field setter.
      */
     @Test
-    public void setTotal() {
+    public void setTotal_Meals() {
         Resource vm = new Resource();
         vm.setTotal_Meals(3);
         Assert.assertEquals(vm.getTotal_Meals(),3);
     }
+
+    /**
+     * Test for the "total" field getter.
+     */
+    @Test
+    public void getTotal_Prduce() {
+        Resource vm = new Resource();
+        vm.setTotal_Produce(2);
+        Assert.assertEquals(vm.getTotal_Produce(),2);
+    }
+
+    /**
+     * Test for the "total" field getter.
+     */
+    @Test
+    public void getTotal_Distance() {
+        Resource vm = new Resource();
+        vm.setTotal_Distance(2);
+        Assert.assertEquals(vm.getTotal_Distance(),2);
+    }
+
+    /**
+     * Test for the "total" field getter.
+     */
+    @Test
+    public void getTotal_Percentage() {
+        Resource vm = new Resource();
+        vm.setTotal_Percentage(2);
+        Assert.assertEquals(vm.getTotal_Percentage(),2);
+    }
+
 }
