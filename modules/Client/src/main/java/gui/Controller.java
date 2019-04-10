91b6ab2035414ca66840328fcc4a84d23b762190
Natália Struharová <n.struharova@student.tudelft.nl>
2019-04-10 15:31:37 +0200

    Change design and labels

--- modules/Client/src/main/java/gui/Controller.java
+++ modules/Client/src/main/java/gui/Controller.java
@@ -35,13 +35,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.RowConstraints;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
